1168e9a7bcea18993908f657c389c08e253a4b35
ShaiYosef <122493094+ShaiYosef@users.noreply.github.com>
2023-02-24 20:14:15 -0700

    Cube

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,247 +1,391 @@
 package com.example.project;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.ArrayList;
+
+
 
 public class Cube {
-	private static char[][][] state = {
-	{
-		{'r', 'r', 'r'},
-		{'r', 'r', 'r'},
-		{'r', 'r', 'r'}
-	},{
-		{'b', 'b', 'b'},
-		{'b', 'b', 'b'},
-		{'b', 'b', 'b'}
-	},{
-		{'o', 'o', 'o'},
-		{'o', 'o', 'o'},
-		{'o', 'o', 'o'}
-	},{
-		{'g', 'g', 'g'},
-		{'g', 'g', 'g'},
-		{'g', 'g', 'g'}
-	},{
-		{'y', 'y', 'y'},
-		{'y', 'y', 'y'},
-		{'y', 'y', 'y'}
-	},{
-		{'w', 'w', 'w'},
-		{'w', 'w', 'w'},
-		{'w', 'w', 'w'}
-	}};
-	public static void main(final String[] args) {
-		Cube cube = new Cube();
-		// Get input from command line arg
-		if (args.length > 1) {
-		  Scanner arg = new Scanner(args[1]);
-		  while (arg.hasNext("((u|d|r|l|f|b)'?)")) {
-			cube.doMove(arg.next("((u|d|r|l|f|b)'?)"));
-		  }
-		  arg.close();
-		}
-		// print cube state
+
+	static String[][] cube = {{
 	
-		for (int i = 0; i < state.length; i++) {
-			for (int j = 0; j < state[i].length; j++) {
-				for (int k = 0; k < state[i][j].length; k++) {
-					System.out.print(state[i][j][k]);
-					if (k == 2) {
-						System.out.print(" ");
-					} else {
-						System.out.print(" | ");
-					}
+		"r", "r", "r",
+		"r", "r", "r",
+		"r", "r", "r"
+		},{
+		"b", "b", "b",
+		"b", "b", "b",
+		"b", "b", "b"
+		},{
+		"o", "o", "o",
+		"o", "o", "o",
+		"o", "o", "o"
+		},{
+		"g", "g", "g",
+		"g", "g", "g",
+		"g", "g", "g"
+		},{
+		"y", "y", "y",
+		"y", "y", "y",
+		"y", "y", "y"
+		},{
+		"w", "w", "w",
+		"w", "w", "w",
+		"w", "w", "w"
+		}
+	};
+
+	class edgedFace{
+		public int current_face;
+		int[] edge1 = new int[4]; //index 0 is the face and 1-3 are the elements of the face
+		int[] edge2 = new int[4]; 
+		int[] edge3 = new int[4];
+		int[] edge4 = new int[4];
+
+		public edgedFace(int face){
+			current_face = face;
+
+			switch(face){
+				case 0:
+				// red face
+				edge1[0] = 1; //blue
+				edge1[1] = 2;
+				edge1[2] = 5;
+				edge1[3] = 8;
+
+				edge2[0] = 4; //yellow
+				edge2[1] = 2;
+				edge2[2] = 5;
+				edge2[3] = 8;
+
+				edge3[0] = 3; // green
+				edge3[1] = 0;
+				edge3[2] = 3;
+				edge3[3] = 6;
+				
+				edge4[0] = 5; // white
+				edge4[1] = 2;
+				edge4[2] = 5;
+				edge4[3] = 8;
+
+				break;
+				
+				case 1:
+				// blue face
+				edge1[0] = 2; // orange
+				edge1[1] = 2;
+				edge1[2] = 5;
+				edge1[3] = 8;
+
+				edge2[0] = 4; // yellow
+				edge2[1] = 6;
+				edge2[2] = 7;
+				edge2[3] = 8;
+
+				edge3[0] = 0; // red
+				edge3[1] = 0;
+				edge3[2] = 3;
+				edge3[3] = 6;
+				
+				edge4[0] = 5; // white
+				edge4[1] = 0;
+				edge4[2] = 1;
+				edge4[3] = 2;
+
+				break;
+
+				case 2:
+				// orange face
+				edge1[0] = 3; // green
+				edge1[1] = 0;
+				edge1[2] = 3;
+				edge1[3] = 6;
+
+				edge2[0] = 4; // yellow
+				edge2[1] = 0;
+				edge2[2] = 3;
+				edge2[3] = 6;
+
+				edge3[0] = 1; // blue
+				edge3[1] = 0;
+				edge3[2] = 3;
+				edge3[3] = 6;
+				
+				edge4[0] = 5; // white
+				edge4[1] = 6;
+				edge4[2] = 7;
+				edge4[3] = 8;
+
+				break;
+
+				case 3:
+				// green face
+				edge1[0] = 0; // red
+				edge1[1] = 2;
+				edge1[2] = 5;
+				edge1[3] = 8;
+
+				edge2[0] = 5; // white
+				edge2[1] = 6;
+				edge2[2] = 7;
+				edge2[3] = 8;
+
+				edge3[0] = 2; // orange
+				edge3[1] = 0;
+				edge3[2] = 3;
+				edge3[3] = 6;
+				
+				edge4[0] = 4; // yellow
+				edge4[1] = 0;
+				edge4[2] = 1;
+				edge4[3] = 2;
+
+				break;
+
+				case 4:
+				// yellow face
+				edge1[0] = 1; // blue
+				edge1[1] = 0;
+				edge1[2] = 1;
+				edge1[3] = 2;
+
+				edge2[0] = 2; // orange
+				edge2[1] = 0;
+				edge2[2] = 1;
+				edge2[3] = 2;
+
+				edge3[0] = 3; // green
+				edge3[1] = 6;
+				edge3[2] = 7;
+				edge3[3] = 8;
+				
+				edge4[0] = 0; // red
+				edge4[1] = 0;
+				edge4[2] = 1;
+				edge4[3] = 2;
+
+				break;
+
+				case 5:
+				// white face
+				edge1[0] = 1; // blue
+				edge1[1] = 6;
+				edge1[2] = 7;
+				edge1[3] = 8;
+
+				edge2[0] = 0; // red
+				edge2[1] = 6;
+				edge2[2] = 7;
+				edge2[3] = 8;
+
+				edge3[0] = 3; // green
+				edge3[1] = 0;
+				edge3[2] = 1;
+				edge3[3] = 2;
+				
+				edge4[0] = 2; // orange
+				edge4[1] = 6;
+				edge4[2] = 7;
+				edge4[3] = 8;
+
+				break;
+
+			}
+		}
+
+	}
+
+	public void turnFace(int index, String direction){
+		edgedFace eFace = new edgedFace(index);
+
+		String[][] copy = new String[6][9];
+
+		for(int i = 0; i<6; i++){
+			for(int j = 0; j<9; j++){
+				copy[i][j] = cube[i][j];
+			}
+		}
+
+		switch(direction){
+			case "c":
+			cube[eFace.current_face][0] = copy[eFace.current_face][2];
+			cube[eFace.current_face][1] = copy[eFace.current_face][5];
+			cube[eFace.current_face][2] = copy[eFace.current_face][8];
+			cube[eFace.current_face][3] = copy[eFace.current_face][1];
+			cube[eFace.current_face][5] = copy[eFace.current_face][7];
+			cube[eFace.current_face][6] = copy[eFace.current_face][0];
+			cube[eFace.current_face][7] = copy[eFace.current_face][3];
+			cube[eFace.current_face][8] = copy[eFace.current_face][6];
+
+			//edge 1 changed
+			cube[eFace.edge1[0]][eFace.edge1[1]] = copy[eFace.edge4[0]][eFace.edge4[1]];
+			cube[eFace.edge1[0]][eFace.edge1[2]] = copy[eFace.edge4[0]][eFace.edge4[2]];
+			cube[eFace.edge1[0]][eFace.edge1[3]] = copy[eFace.edge4[0]][eFace.edge4[3]];
+
+			//edge 2 changed
+			cube[eFace.edge2[0]][eFace.edge2[1]] = copy[eFace.edge1[0]][eFace.edge1[1]];
+			cube[eFace.edge2[0]][eFace.edge2[2]] = copy[eFace.edge1[0]][eFace.edge1[2]];
+			cube[eFace.edge2[0]][eFace.edge2[3]] = copy[eFace.edge1[0]][eFace.edge1[3]];
+
+			//edge 3 changed
+			cube[eFace.edge3[0]][eFace.edge3[1]] = copy[eFace.edge2[0]][eFace.edge2[1]];
+			cube[eFace.edge3[0]][eFace.edge3[2]] = copy[eFace.edge2[0]][eFace.edge2[2]];
+			cube[eFace.edge3[0]][eFace.edge3[3]] = copy[eFace.edge2[0]][eFace.edge2[3]];
+
+			//edge 4 changed
+			cube[eFace.edge4[0]][eFace.edge4[1]] = copy[eFace.edge3[0]][eFace.edge3[1]];
+			cube[eFace.edge4[0]][eFace.edge4[2]] = copy[eFace.edge3[0]][eFace.edge3[2]];
+			cube[eFace.edge4[0]][eFace.edge4[3]] = copy[eFace.edge3[0]][eFace.edge3[3]];
+
+
+
+			break;
+
+			case "cc":
+			cube[eFace.current_face][0] = copy[eFace.current_face][2];
+			cube[eFace.current_face][1] = copy[eFace.current_face][5];
+			cube[eFace.current_face][2] = copy[eFace.current_face][8];
+			cube[eFace.current_face][3] = copy[eFace.current_face][1];
+			cube[eFace.current_face][5] = copy[eFace.current_face][7];
+			cube[eFace.current_face][6] = copy[eFace.current_face][0];
+			cube[eFace.current_face][7] = copy[eFace.current_face][3];
+			cube[eFace.current_face][8] = copy[eFace.current_face][6];
+
+			//edge 1 changed
+			cube[eFace.edge1[0]][eFace.edge1[1]] = copy[eFace.edge2[0]][eFace.edge2[1]];
+			cube[eFace.edge1[0]][eFace.edge1[2]] = copy[eFace.edge2[0]][eFace.edge2[2]];
+			cube[eFace.edge1[0]][eFace.edge1[3]] = copy[eFace.edge2[0]][eFace.edge2[3]];
+
+			//edge 2 changed
+			cube[eFace.edge2[0]][eFace.edge2[1]] = copy[eFace.edge3[0]][eFace.edge3[1]];
+			cube[eFace.edge2[0]][eFace.edge2[2]] = copy[eFace.edge3[0]][eFace.edge3[2]];
+			cube[eFace.edge2[0]][eFace.edge2[3]] = copy[eFace.edge3[0]][eFace.edge3[3]];
+
+			//edge 3 changed
+			cube[eFace.edge3[0]][eFace.edge3[1]] = copy[eFace.edge4[0]][eFace.edge4[1]];
+			cube[eFace.edge3[0]][eFace.edge3[2]] = copy[eFace.edge4[0]][eFace.edge4[2]];
+			cube[eFace.edge3[0]][eFace.edge3[3]] = copy[eFace.edge4[0]][eFace.edge4[3]];
+
+			//edge 4 changed
+			cube[eFace.edge4[0]][eFace.edge4[1]] = copy[eFace.edge1[0]][eFace.edge1[1]];
+			cube[eFace.edge4[0]][eFace.edge4[2]] = copy[eFace.edge1[0]][eFace.edge1[2]];
+			cube[eFace.edge4[0]][eFace.edge4[3]] = copy[eFace.edge1[0]][eFace.edge1[3]];
+
+			break;
+		}
+
+
+	}
+
+	public void showCube(){
+		int ind = 0;
+		for(int x=0; x<6; x++){
+			for(int y=0; y<3; y++){
+				for(int z=0; z<3; z++){
+					System.out.print(cube[x][ind++]);
 				}
 				System.out.println();
 			}
-			if (i != state.length - 1) {
-				System.out.println();
-			}
+			ind=0;
+			System.out.println();
 		}
-		Scanner keyboard = new Scanner(System.in);
-		System.out.print("> ");
-		while (keyboard.hasNextLine()) {
-			System.out.print("> ");
-		  	if (keyboard.hasNext("quit")) {
+	}
+
+	public static void main(String[] args) 
+	throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+		Cube RubiksCube = new Cube();
+
+
+
+		boolean argsCheck = false;
+		int argsRunIndex = 0;
+
+		if(args.length > 0){
+			argsCheck = true;
+		}
+
+		boolean proceed = true;
+
+		while(proceed){
+			String input;
+
+			if(argsCheck){
+				input = reader.readLine();
+			}else{
+				if(argsRunIndex == args.length){
+					argsCheck = false;
+					input = "s";
+				}else{
+					input = args[argsRunIndex];
+					argsRunIndex++;
+				}
+			}
+
+			switch(input){
+				case "u":
+				RubiksCube.turnFace(4, "c");
+				RubiksCube.showCube();
 				break;
-		  	}
-		  	while (keyboard.hasNext("((u|d|r|l|f|b)'?)")) {
-				cube.doMove(keyboard.next("((u|d|r|l|f|b)'?)"));
-		  	}
-		keyboard.nextLine();
-		// print cube state
-		
-
-		
-		}
-		keyboard.close();
-	  }
-
-
-	  
-
-	// Method containing moves
-	public void doMove(String moveString){
-		switch (moveString) {
-			case "u":
-			doMoveU();
-			break;
-			case "d":
-			doMoveD();
-			break;
-			case "r":
-			doMoveR();
-			break;
-			case "l":
-			doMoveL();
-			break;
-			case "f":
-			doMoveF();
-			break;
-			case "b":
-			doMoveB();
-			break;
-			case "u'":
-			doMoveU();
-			doMoveU();
-			doMoveU();
-			break;
-			case "d'":
-			doMoveD();
-			doMoveD();
-			doMoveD();
-			break;
-			case "r'":
-			doMoveR();
-			doMoveR();
-			doMoveR();
-			break;
-			case "l'":
-			doMoveL();
-			doMoveL();
-			doMoveL();
-			break;
-			case "f'":
-			doMoveF();
-			doMoveF();
-			doMoveF();
-			break;
-			case "b'":
-			doMoveB();
-			doMoveB();
-			doMoveB();
-			break;
-		}
-	}
-	
-	// Method for rotating faces as matrices
-	public static char[][] rotateMatrix(char[][] matrix){
-		int height = matrix.length;
-        int width = matrix[0].length;
-        char[][] rotatedMatrix = new char[width][height];
-        
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                rotatedMatrix[j][(height-1)-i] = matrix[i][j];
-            }
-        }
-        return rotatedMatrix;
-	}
-
-	// Method for referencing columns
-	public static char[] getColumn(char[][] matrix, int index){
-		int width = matrix[0].length;
-        char[] column = new char[width];
-
-        for (int i = 0; i < width; i++) {
-            column[i] = matrix[index][i];
-        }
-        return column;
-	}
-
-	// Method for setting new columns
-	private static void setColumn(char[][] src, int srcIdx, char[][] dest, int destIdx) {
-        int srcColumns = src[0].length;
-
-        for (int i = 0; i < srcColumns; i++) {
-            dest[destIdx][i] = src[srcIdx][i];
-        }
-	}
-
-	private static void setColumn(char[] column, char[][] matrix, int index) {
-        int sourceLength = column.length;
-
-        for (int i = 0; i < sourceLength; i++) {
-            matrix[index][i] = column[i];
-        }
-    }
-
-	private static char[][] reverseRotateMatrix(char[][] matrix) {
-		int height = matrix.length;
-		int width = matrix[0].length;
-		char[][] rrMatrix = new char[width][height];
-			
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				rrMatrix[(width-1)-j][i] = matrix[i][j];
+				case "d":
+				RubiksCube.turnFace(5, "c");
+				RubiksCube.showCube();
+				break;
+				case "r":
+				RubiksCube.turnFace(0, "c");
+				RubiksCube.showCube();
+				break;
+				case "l":
+				RubiksCube.turnFace(2, "c");
+				RubiksCube.showCube();
+				break;
+				case "f":
+				RubiksCube.turnFace(1, "c");
+				RubiksCube.showCube();
+				break;
+				case "b":
+				RubiksCube.turnFace(3, "c");
+				RubiksCube.showCube();
+				break;
+				case "u'":
+				RubiksCube.turnFace(4, "cc");
+				RubiksCube.showCube();
+				break;
+				case "d'":
+				RubiksCube.turnFace(5, "cc");
+				RubiksCube.showCube();
+				break;
+				case "r'":
+				RubiksCube.turnFace(0, "cc");
+				RubiksCube.showCube();
+				break;
+				case "l'":
+				RubiksCube.turnFace(2, "cc");
+				RubiksCube.showCube();
+				break;
+				case "f'":
+				RubiksCube.turnFace(1, "c");
+				RubiksCube.showCube();
+				break;
+				case "b'":
+				RubiksCube.turnFace(3, "c");
+				RubiksCube.showCube();
+				break;
+				case "s":
+				break;
+				case "q":
+				proceed = false;
+				break;
 			}
 		}
-		return rrMatrix;
+		
 	}
-
-	public void doMoveU(){
-		state[4] = reverseRotateMatrix(state[4]);
-		char[] temp = getColumn(state[2], 0);
-		setColumn(state[1], 0, state[2], 0);
-		setColumn(state[0], 0, state[1], 0);
-		setColumn(state[3], 0, state[0], 0);
-		setColumn(temp, state[3], 0);
-	}
-
-	public void doMoveD(){
-		state[5] = reverseRotateMatrix(state[5]);
-		char[] temp = getColumn(state[3], 2);
-		setColumn(state[0], 2, state[3], 2);
-		setColumn(state[1], 2, state[0], 2);
-		setColumn(state[2], 2, state[1], 2);
-		setColumn(temp, state[2], 2);
-	}
-
-	public void doMoveR(){
-		state[0] = reverseRotateMatrix(state[0]);
-		char[] temp = state[3][0];
-		state[3][0] = state[4][2];
-		state[4][2] = state[1][2];
-		state[1][2] = state[5][2];
-		state[5][2] = temp;
-	}
-	
-	public void doMoveL(){
-		state[2] = reverseRotateMatrix(state[2]);
-		char[] temp = state[3][2];
-		state[3][2] = state[5][0];
-		state[5][0] = state[1][0];
-		state[1][0] = state[4][0];
-		state[4][0] = temp;
-	}
-
-	public void doMoveF(){
-		state[1] = reverseRotateMatrix(state[1]);
-		char[] temp = getColumn(state[5], 0);
-		setColumn(state[0][0], state[5], 0);
-		state[0][0] = getColumn(state[4], 2);
-		setColumn(state[2][2], state[4], 2);
-		state[2][2] = temp;
-	}
-
-	public void doMoveB(){
-		state[3] = reverseRotateMatrix(state[3]);
-		char[] temp = getColumn(state[4], 0);
-		setColumn(state[0][2], state[4], 0);
-		state[0][2] = getColumn(state[5], 2);
-		setColumn(state[2][0], state[5], 2);
-		state[2][0] = temp;
-	}
-	
 
 }
-
